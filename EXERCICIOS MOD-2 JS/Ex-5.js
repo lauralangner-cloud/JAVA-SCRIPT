c742f73ef66643b023ee875ded1b22d0fd79b71c
LAURA DOS SANTOS LANGNER <laura_langner@estudante.sesisenai.org.br>
2026-05-28 22:07:17 -0300

    Finalizando exercicios mod 2- Iniciando o desafio o 1° desafio do mod 2A- Analisador de turma

---
+++ EXERCICIOS MOD-2 JS/Ex-5.js
@@ -0,0 +1,5 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "Bootstrap", "Node.js"];
+
+    for(let i = 0; i < tecnologias.length; i++){
+        console.log(`POSIÇÃO [${i}]: ITEM [${tecnologias[i]}]`)
+    }
